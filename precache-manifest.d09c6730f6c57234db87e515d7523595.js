2f49427625befca548ef359919d45b64898758e9
liu00david <46476073+liu00david@users.noreply.github.com>
2023-03-19 03:48:30 +0000

    Deploying to gh-pages from @ liu00david/davidwideweb@fbec4436cedee6b76f7a28247bab9a9fb8a849c5 🚀

---
+++ precache-manifest.d09c6730f6c57234db87e515d7523595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "237229a7a8a001716c2b",
+    "url": "/davidwideweb/static/css/main.99074e9a.chunk.css"
+  },
+  {
+    "revision": "237229a7a8a001716c2b",
+    "url": "/davidwideweb/static/js/main.237229a7.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/davidwideweb/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "4ae1313b7e83cd0cdd32",
+    "url": "/davidwideweb/static/js/2.4ae1313b.chunk.js"
+  },
+  {
+    "revision": "d3146ee53f3f75a1bfce",
+    "url": "/davidwideweb/static/js/runtime~main.d3146ee5.js"
+  },
+  {
+    "revision": "b7c35a66422237c44e2db35d171c5eb8",
+    "url": "/davidwideweb/index.html"
+  }
+];
