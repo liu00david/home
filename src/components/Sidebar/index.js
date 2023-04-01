80812adaf072755afeb5459e837e55b21855e9ce
David Liu <david.liu@yahooinc.com>
2023-04-01 09:47:19 -0500

    '

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -12,7 +12,7 @@ const Sidebar = ({ isOpen, toggle }) => {
       {isOpen ?
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to="about" onClick={toggle}>About</SidebarLink>
+          <SidebarLink to="about" onClick={toggle}>About DAVID</SidebarLink>
           <SidebarLink to="discover" onClick={toggle}>Discover</SidebarLink>
           <SidebarLink to="services" onClick={toggle}>Services</SidebarLink>
           <SidebarLink to="signup" onClick={toggle}>Sign Up</SidebarLink>
