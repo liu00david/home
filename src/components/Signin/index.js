a4738329047584ce2f8d5fea27d5d123b272d76e
David Liu <david.liu@yahooinc.com>
2023-04-14 00:45:27 -0500

    fixed

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
-// import { SigninContainer, SigninWrapper } from '../Signin/SigninElements'
+import { SigninContainer, SigninWrapper } from './SigninElements'
 
 const Signin = () => {
   return (
-    <>hi</>
-    // <SigninContainer>
-    //   <SigninWrapper>
-    //     ARE YOU READY
-    //   </SigninWrapper>
-    // </SigninContainer>
+    <SigninContainer>
+      <SigninWrapper>
+        ARE YOU READY
+      </SigninWrapper>
+    </SigninContainer>
   )
 }
 
