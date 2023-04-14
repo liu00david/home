bc832e397ca2475a1eb0489e7210f1a76afdb0df
David Liu <david.liu@yahooinc.com>
2023-04-14 16:19:41 -0500

    give up parallax

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Video from '../../videos/background.mp4'
 // import { Button } from '../ButtonElement';
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP } from './HeroElements'
+import { Parallax, Background } from 'react-parallax';
 
 const HeroSection = () => {
 //   const [hover, setHover] = useState(true)
@@ -28,6 +29,7 @@ const HeroSection = () => {
                     who {hover ? <ArrowForward /> : <ArrowRight />}
                 </Button>
             </HeroBtnWrapper> */}
+
         </HeroContent>
     </HeroContainer>
   )
