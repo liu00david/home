fb91e2e846104960000db3de2e5f8e1bebcb4f08
liu00david <46476073+liu00david@users.noreply.github.com>
2023-04-20 21:57:45 +0000

    Deploying to gh-pages from @ liu00david/home@b1875c1decefe2aa6d38379a2175e1b1227e1be0 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.688f709ff43c812a9de6a7bf0bb8e0bd.js"
+  "/home/precache-manifest.65caff4588192a836b061974a95c0bf6.js"
 );
 
 workbox.clientsClaim();
